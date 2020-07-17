1b69e2bcbc514d76c46cafeb2c87257c243dd8f5
mainul786 <53607383+mainul786@users.noreply.github.com>
2020-07-17 23:01:37 +0530

    assignment sucefully

---
+++ assignment.js
@@ -0,0 +1,54 @@
+
+// 1.feetToMile convert 
+function feetToMile(feet){
+    var mile=feet/5280;
+    return mile;
+}
+
+// 2.woodCalculator
+function woodCalculator(chair,table,bed){
+    var charCount=chair*1;
+    var tableCount=table*3;
+    var bedCount=bed*5;
+    var totalWood=charCount+tableCount+bedCount;
+    return totalWood;
+}
+
+
+
+//3.brickCalculator
+
+function brickCalculator(floor){
+    if(floor<=10){
+        var bellowTen=floor * 15
+        var totalBrickSmall=bellowTen * 1000;
+        return totalBrickSmall;
+    }else if(floor>=11 && floor<=20){
+        var aboveTen=floor-10;
+        var aboveTenfloor=floor - aboveTen;
+        var aboveTenfloorBrick=aboveTenfloor * 15 + aboveTen * 12;
+        var totalBrickForMedium=aboveTenfloorBrick * 1000;
+        return totalBrickForMedium;
+    }
+    else{
+        var buldingTop=floor-20;
+        var buildingMiddle=10;
+        var buildingBottom=10;
+        var highestBuilding=buildingBottom * 15 + buildingMiddle * 12 + buldingTop * 10;
+        var totalBrickForHigh=highestBuilding * 1000;
+        return totalBrickForHigh;
+    }
+}
+
+
+// 4.tinyFriend
+function tinyFriend(names){
+    var smallest=names[0];
+    for(var i=0;i<names.length;i++){
+        var countnameslength=names[i];
+        if(countnameslength.length<smallest.length){
+            smallest=countnameslength;
+        }
+    }
+    return smallest;
+}
